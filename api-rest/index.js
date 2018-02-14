6212f6f9a587b561817a0242eeb6a67268a3ef77
Chriscalde <chris.calderon.ortega@gmail.com>
2018-02-14 08:36:59 -0700

    Rutas y Parametros

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,11 +1,51 @@
 var express = require('express');
+var bodyparser= require('body-parser');
 var app = express();
+
+app.use(bodyparser.json());
+
+app.get('/suma/:no1/:no2',function(req,res){
+    no1 = req.params.no1;
+    no2 = req.params.no2;
+    n1= parseInt(no1);
+    n2= parseInt(no2);
+    var sum= n1+n2;
+    res.status(200).send({Suma:sum});
+});
+
+app.get('/resta/:no1/:no2',function(req,res){
+    no1 = req.params.no1;
+    no2 = req.params.no2;
+    n1= parseInt(no1);
+    n2= parseInt(no2);
+    var sum= n1-n2;
+    res.status(200).send({Resta:sum});
+});
+
+app.get('/division/:no1/:no2',function(req,res){
+    no1 = req.params.no1;
+    no2 = req.params.no2;
+    n1= parseInt(no1);
+    n2= parseInt(no2);
+    var sum= n1/n2;
+    res.status(200).send({Division:sum});
+});
+
+app.get('/multiplicacion/:no1/:no2',function(req,res){
+    no1 = req.params.no1;
+    no2 = req.params.no2;
+    n1= parseInt(no1);
+    n2= parseInt(no2);
+    var sum= n1*n2;
+    res.status(200).send({Multiplicacion:sum});
+});
 
 var server= app.listen(3678,function(){
     var host= server.address().address;
     var port= server.address().port;
     console.log('App listening at http://localhost ',port);
 });
+
 //ES6 es el mas reciente estandar del lenguaje de programacion Javascript. Define como debe de ser el lenguaje Javascript.
 //'var' es cuando no se reasigna la variable y no se usa para toda la funcion.
 //'const' es cuando el identificador no sera reasignado.
